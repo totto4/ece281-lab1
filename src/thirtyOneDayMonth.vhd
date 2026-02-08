f36575db3dbb4ed004071c0bb1c0e8145602f1bb
totto4 <225091499+totto4@users.noreply.github.com>
2026-02-07 23:26:30 -0700

    Complete, Basys Board Working

--- src/thirtyOneDayMonth.vhd
+++ src/thirtyOneDayMonth.vhd
@@ -56,24 +56,35 @@ library ieee;
 entity thirtyOneDayMonth is 
   port(
 	i_A : in std_logic; -- one of four inputs
-	
-	
-	
-						-- output
+	i_B : in std_logic;
+	i_C : in std_logic;
+	i_D : in std_logic;
+	o_Y : out std_logic					-- output
   );
 end thirtyOneDayMonth;
 
 architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is 
 	-- include components declarations and signals
-	
+	signal w_sel : std_logic_vector (2 downto 0);
 	--signals internal to the architecture are declared and initialized such as w_sel
   
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
 	w_sel(0) <= i_C;	-- one
+	w_sel(1) <= i_B;
+	w_sel(2) <= i_A;
 	--finish assigning signals
 	
 	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
+	o_Y <= i_D when (w_sel = "000") else 
+           i_D when (w_sel = "001") else
+           i_D when (w_sel = "010") else 
+           i_D when (w_sel = "011") else 
+           not i_D when (w_sel = "100") else
+           not i_D when (w_sel = "101") else
+           not i_D when (w_sel = "110") else
+           not i_D when (w_sel = "111") else
+           '0'; 
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
